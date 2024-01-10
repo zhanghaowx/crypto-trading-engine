be21f08d17830a86ed829eac89013f3a889a6821
Hao Zhang <zhanghaowx@gmail.com>
2024-01-10 13:53:43 -0600

    Add docs to PositionManager

--- crypto_trading_engine/market_data/common/candlestick.py
+++ crypto_trading_engine/market_data/common/candlestick.py
@@ -8,7 +8,7 @@ class Candlestick:
     def __init__(self, start: datetime, duration_in_seconds: float):
         """
         Candlestick displays the high, low, open, and close price of a
-        security for a specific period.
+        security/crypto currency for a specific period.
 
         Args:
             start: Start time that the candlestick represents
@@ -16,9 +16,7 @@ class Candlestick:
                                  candlestick represents
         """
         self.start_time: datetime = start
-        self.end_time: datetime = start + timedelta(
-            seconds=duration_in_seconds
-        )
+        self.end_time: datetime = start + timedelta(seconds=duration_in_seconds)
         self.open: float = 0.0
         self.high: float = 0.0
         self.low: float = float("inf")
@@ -57,10 +55,7 @@ class Candlestick:
             True if trade is successfully added to the candlestick. Otherwise
             False
         """
-        if (
-            transaction_time < self.start_time
-            or transaction_time > self.end_time
-        ):
+        if transaction_time < self.start_time or transaction_time > self.end_time:
             return False
 
         if self.volume == 0.0:
--- crypto_trading_engine/position/position_manager.py
+++ crypto_trading_engine/position/position_manager.py
@@ -16,6 +16,16 @@ class PositionManager:
         self.positions: dict[str, Position] = {}
 
     def on_buy(self, symbol: str, price: float, quantity: float):
+        """
+        Adds the buy trade to tracked positions
+
+        Args:
+            symbol: Symbol of the security/cryptocurrency just traded
+            price: Price of the trade
+            quantity: Quantity of the trade
+        Returns:
+            Current position of the traded symbol
+        """
         self.positions[symbol] = (
             self.positions[symbol]
             if symbol in self.positions.keys()
@@ -25,8 +35,19 @@ class PositionManager:
         self.positions[symbol].cash_value += price * quantity
 
         assert symbol in self.positions.keys()
+        return self.positions[symbol]
 
     def on_sell(self, symbol: str, price: float, quantity: float):
+        """
+        Removes the sell trade from tracked positions
+
+        Args:
+            symbol: Symbol of the security/cryptocurrency just traded
+            price: Price of the trade
+            quantity: Quantity of the trade
+        Returns:
+            Current position of the traded symbol
+        """
         assert symbol in self.positions.keys()
 
         self.positions[symbol].volume -= quantity
@@ -34,3 +55,4 @@ class PositionManager:
 
         assert self.positions[symbol].volume >= 0
         assert self.positions[symbol].cash_value >= 0
+        return self.positions[symbol]
