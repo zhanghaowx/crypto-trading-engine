74d746971588cf51a2c83979ade31a924156e257
Hao Zhang <zhanghaowx@gmail.com>
2024-01-14 10:42:26 -0600

    Add unit test to guard against changes to side.py

---
+++ tests/core/test_side.py
@@ -0,0 +1,28 @@
+import unittest
+from enum import Enum
+
+from crypto_trading_engine.core.side import MarketSide
+
+
+class TestMarketSideEnum(unittest.TestCase):
+    def test_enum_name(self):
+        # Setting name and value to be equal to fix an unknown bug that a
+        # MarketSide might be implicitly converted to a string.
+
+        self.assertEqual(MarketSide.BUY.value, MarketSide.BUY.name)
+        self.assertEqual(MarketSide.SELL.value, MarketSide.SELL.name)
+
+    def test_buy_enum_value(self):
+        self.assertEqual(MarketSide.BUY.value, "BUY")
+
+    def test_sell_enum_value(self):
+        self.assertEqual(MarketSide.SELL.value, "SELL")
+
+    def test_enum_membership(self):
+        self.assertTrue(isinstance(MarketSide.BUY, Enum))
+        self.assertTrue(isinstance(MarketSide.SELL, Enum))
+
+    def test_enum_equality(self):
+        self.assertEqual(MarketSide.BUY, MarketSide.BUY)
+        self.assertEqual(MarketSide.SELL, MarketSide.SELL)
+        self.assertNotEqual(MarketSide.BUY, MarketSide.SELL)
