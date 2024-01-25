3c1a20f90bcd8dbe49ceb8c814efd964ab4b7cc0
Hao Zhang <zhanghaowx@gmail.com>
2024-01-25 13:43:50 -0600

    App: add a new option

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -18,7 +18,7 @@ class TestCryptoTradingEngineCLI(unittest.IsolatedAsyncioTestCase):
             exchange="Coinbase",
         ),
     )
-    async def test_main_run_once_mode(self, mock_args, MockApplication):
+    async def test_main_run_coinbase_replay(self, mock_args, MockApplication):
         mock_app = MockApplication.return_value
         mock_app.run_replay = AsyncMock()
         mock_app.run_replay.return_value = 1.0
@@ -44,7 +44,7 @@ class TestCryptoTradingEngineCLI(unittest.IsolatedAsyncioTestCase):
             exchange="Kraken",
         ),
     )
-    async def test_main_run_once_mode(self, mock_args, MockApplication):
+    async def test_main_run_kraken_replay(self, mock_args, MockApplication):
         mock_app = MockApplication.return_value
         mock_app.run_replay = AsyncMock()
         mock_app.run_replay.return_value = 1.0
@@ -61,7 +61,6 @@ class TestCryptoTradingEngineCLI(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(1, mock_app.run_replay.call_count)
         self.assertEqual("", captured_output.getvalue().split("\n")[-1])
 
-    @patch("jolteon.app.kraken.KrakenApplication")
     @patch(
         "argparse.ArgumentParser.parse_args",
         return_value=argparse.Namespace(
@@ -70,7 +69,7 @@ class TestCryptoTradingEngineCLI(unittest.IsolatedAsyncioTestCase):
             exchange="Mock",
         ),
     )
-    async def test_main_run_once_mode(self, mock_args, MockApplication):
+    async def test_main_run_mock_exchange_replay(self, mock_args):
         with self.assertRaises(NotImplementedError) as context:
             await main()
 
@@ -78,6 +77,58 @@ class TestCryptoTradingEngineCLI(unittest.IsolatedAsyncioTestCase):
             str(context.exception),
             "Application is not implemented for market Mock",
         )
+
+    @patch("jolteon.app.coinbase.CoinbaseApplication")
+    @patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            replay_start="",
+            replay_end="",
+            exchange="Coinbase",
+        ),
+    )
+    async def test_main_run_coinbase_live(self, mock_args, MockApplication):
+        mock_app = MockApplication.return_value
+        mock_app.start = AsyncMock()
+        mock_app.start.return_value = 1.0
+
+        # Redirect stdout to capture output
+        captured_output = StringIO()
+        sys.stdout = captured_output
+
+        await main()
+
+        # Reset stdout
+        sys.stdout = sys.__stdout__
+
+        self.assertEqual(1, mock_app.start.call_count)
+        self.assertEqual("", captured_output.getvalue().split("\n")[-1])
+
+    @patch("jolteon.app.kraken.KrakenApplication")
+    @patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            replay_start="",
+            replay_end="",
+            exchange="Kraken",
+        ),
+    )
+    async def test_main_run_kraken_live(self, mock_args, MockApplication):
+        mock_app = MockApplication.return_value
+        mock_app.start = AsyncMock()
+        mock_app.start.return_value = 1.0
+
+        # Redirect stdout to capture output
+        captured_output = StringIO()
+        sys.stdout = captured_output
+
+        await main()
+
+        # Reset stdout
+        sys.stdout = sys.__stdout__
+
+        self.assertEqual(1, mock_app.start.call_count)
+        self.assertEqual("", captured_output.getvalue().split("\n")[-1])
 
     async def test_graceful_exit(self):
         # Redirect stdout to capture output
